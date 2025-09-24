46112fd1a88cb4def0a20204ccc9dd9dc2bcef17
Anuar Turlubay <turlybay.anuar909@gmail.com>
2025-09-24 23:22:22 +0500

    initial

--- vue-project/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { init } from '@telegram-apps/sdk-vue'
-
-// Инициализация Telegram Web App
-init()
-
-createApp(App).mount('#app')
